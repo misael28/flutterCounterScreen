291869ceb947920f2dcf0c8200edfddca2851eaf
Souza <38145379+misael28@users.noreply.github.com>
2021-04-01 20:40:55 -0300

    feat: add remove button at counter screen

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class CounterScreen extends StatefulWidget {
 
 class _CounterScreenState extends State<CounterScreen> {
   var resultado = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,24 +29,42 @@ class _CounterScreenState extends State<CounterScreen> {
         children: [
           Center(
             child: 
-            Text('Botão pressionado: ', 
+            Text('Voce pressionou o botão: ', 
             style: TextStyle(
             fontSize: 24
           ),
           ),
           ),
-          Center(child: Text('$resultado', style: TextStyle(fontSize: 30),))
+          Center(child: Text("${(resultado>1) ? '$resultado vezes' : '$resultado vez' }", style: TextStyle(fontSize: 30),))
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Botao pressionado');
-          setState((){
-            resultado += 1;
-          });
-        },
-        child: Icon(Icons.add),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              print('Botao pressionado');
+              setState((){
+                resultado += 1;
+              });
+            },
+            child: Icon(Icons.add),
       ),
+        FloatingActionButton(
+            onPressed: () {
+              print('Botao pressionado');
+              setState((){
+                if(resultado<=0){
+                  resultado = 0;
+                }else {
+                  resultado --;
+                } 
+              });
+            },
+            child: Icon(Icons.remove),
+      )
+        ],
+      )
     );
   }
 }
